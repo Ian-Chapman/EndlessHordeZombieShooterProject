bca3193989f37c1f9e0f224c3a59e8e203a13606
Ian Chapman <e-n_chapman123@hotmail.com>
2022-04-05 01:35:53 -0400

    Week 12 Lab: Prepping scriptable objects (health and weapon pickups) for inventory use

---
+++ Assets/[Scripts]/ScriptableObjects/ConsumableScriptable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Consumable", menuName = "Items/Consumable", order = 1)]
+public class ConsumableScriptable : ItemScript
+{
+    public int effect = 0;
+
+    public override void UseItem(PlayerController playerController)
+    {
+        //check to see if player is at max health, return
+        //Heal player with potion/first aid kit
+       
+        
+        SetAmount(amountValue - 1);
+        //if item is used, remove it. If item value is below 0, remove it from the inventory
+        if (amountValue <= 0)
+        {
+            DeleteItem(playerController);
+        }
+    }
+}
+
---
+++ Assets/[Scripts]/ScriptableObjects/EquipableScriptable.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EquipableScriptable : ItemScript
+{
+    public bool Equipped {
+        get => isEquipped;
+        set
+        {
+            Equipped = value;
+            OnEquipStatusChange?.Invoke();
+        }
+    }
+
+    public delegate void EquipStatusChange();
+    public event EquipStatusChange OnEquipStatusChange;
+
+    bool isEquipped = false;
+
+    public override void UseItem(PlayerController playerController)
+    {
+        Equipped = !Equipped;
+    }
+}
